a4a0b557a3a805b218cb6fb6c1d473acf8e0a93e
Nicolas Castel <nic.castel@gmail.com>
2026-09-08 10:53:02 +0200

    Update run_open_starnet.sh

--- run_open_starnet.sh
+++ run_open_starnet.sh
@@ -2,13 +2,11 @@
 echo $@ > args.txt
 
 # Loop through arguments
-while [ "$#" -gt 0 ]; do
-  case "$1" in
-    -v|--version)
-      echo "starnet1  version: 1.0.0"
-      exit 0
-      ;;
-  esac
-done
+case "$1" in
+  -v|--version)
+    echo "starnet1  version: 1.0.0"
+    exit 0
+    ;;
+esac
 
 python3.12 open_starnet.py $@
