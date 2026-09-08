b28bc26fcdd79b36ad2cc3374080c6285fbf4f3f
Nicolas Castel <nic.castel@gmail.com>
2026-09-08 16:15:05 +0200

    Add error logging int oa file

--- run_open_starnet.sh
+++ run_open_starnet.sh
@@ -4,9 +4,12 @@ echo $@ > args.txt
 # Loop through arguments
 case "$1" in
   -v|--version)
-    echo "starnet1  version: 1.0.0"
+    echo "StarNet++ v2"
+    #echo "starnet2  version: 1.0.0"
     exit 0
     ;;
 esac
 
-python3.12 open_starnet.py $@
+echo "MPS backend"
+python3.12 open_starnet.py $@ 2> error.log
+exit 0
